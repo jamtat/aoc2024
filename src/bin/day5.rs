b9bf40e87980fcdefce18cbdf0d7aa04cbc177d1
Ben Tattersley <ubershinysheep@gmail.com>
2024-12-07 20:42:33 +0000

    Day 5 Part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,11 +1,11 @@
 use aoc2024::aoc;
 use std::collections::{HashMap, HashSet};
 
-fn page_valid(page: &[usize], rules_after: &HashMap<usize, HashSet<usize>>) -> bool {
-    for i in 0..page.len() - 1 {
-        let n = page[i];
-        let rest = &page[i + 1..];
-        if let Some(banned_pages) = rules_after.get(&n) {
+fn update_valid(update: &[usize], rules_after: &HashMap<usize, HashSet<usize>>) -> bool {
+    for i in 0..update.len() - 1 {
+        let page = update[i];
+        let rest = &update[i + 1..];
+        if let Some(banned_pages) = rules_after.get(&page) {
             for nn in rest {
                 if banned_pages.contains(nn) {
                     return false;
@@ -16,17 +16,16 @@ fn page_valid(page: &[usize], rules_after: &HashMap<usize, HashSet<usize>>) -> b
 
     true
 }
-
 struct Input {
-    pub rules_before: HashMap<usize, HashSet<usize>>,
+    pub _rules_before: HashMap<usize, HashSet<usize>>,
     pub rules_after: HashMap<usize, HashSet<usize>>,
-    pub pages: Vec<Vec<usize>>,
+    pub updates: Vec<Vec<usize>>,
 }
 
 fn parse_input(input: &str) -> Input {
-    let (rules, pages) = input.split_once("\n\n").unwrap();
+    let (rules, updates) = input.split_once("\n\n").unwrap();
 
-    let (rules_before, rules_after): (
+    let (_rules_before, rules_after): (
         HashMap<usize, HashSet<usize>>,
         HashMap<usize, HashSet<usize>>,
     ) = rules
@@ -45,16 +44,16 @@ fn parse_input(input: &str) -> Input {
             },
         );
 
-    let pages: Vec<Vec<usize>> = pages
+    let updates: Vec<Vec<usize>> = updates
         .trim()
         .lines()
         .map(|l| l.split(',').map(|s| s.parse().unwrap()).collect())
         .collect();
 
     Input {
-        rules_before,
+        _rules_before,
         rules_after,
-        pages,
+        updates,
     }
 }
 
@@ -63,16 +62,15 @@ mod part1 {
 
     pub fn calculate(input: &Input) -> usize {
         input
-            .pages
+            .updates
             .iter()
-            .filter(|page| page_valid(page, &input.rules_after))
-            .map(|page| page[page.len() / 2])
+            .filter(|update| update_valid(update, &input.rules_after))
+            .map(|update| update[update.len() / 2])
             .sum()
     }
 
     #[cfg(test)]
     mod test {
-
         use super::*;
 
         #[test]
@@ -84,6 +82,93 @@ mod part1 {
     }
 }
 
+mod part2 {
+    use super::*;
+
+    pub fn calculate(input: &Input) -> usize {
+        input
+            .updates
+            .iter()
+            .filter_map(move |update| fix(update, &input.rules_after))
+            .map(|update| update[update.len() / 2])
+            .sum()
+    }
+
+    fn fix(update: &[usize], rules_after: &HashMap<usize, HashSet<usize>>) -> Option<Vec<usize>> {
+        let mut out = Vec::from(update);
+        let l = out.len();
+        if update_valid(&out, rules_after) {
+            #[cfg(test)]
+            println!("Update already valid! {out:?}");
+            return None;
+        }
+        while !update_valid(&out, rules_after) {
+            for i in 0..l - 1 {
+                let mut update_idx: Option<usize> = None;
+                let page = out[i];
+                let Some(rules) = rules_after.get(&page) else {
+                    #[cfg(test)]
+                    println!("No rules found for {page}");
+                    continue;
+                };
+                #[cfg(test)]
+                println!("Checking for swaps for {page} at {i}");
+
+                for j in i + 1..l {
+                    let pj = out[j];
+                    #[cfg(test)]
+                    println!("  Checking for swap against {pj} at j");
+                    if rules.contains(&pj) {
+                        #[cfg(test)]
+                        println!(
+                            "    {} cannot have {} after, setting swap to {}",
+                            page, pj, j
+                        );
+                        update_idx = Some(j);
+                    }
+                }
+                if let Some(update_idx) = update_idx {
+                    #[cfg(test)]
+                    println!("  Moving {page} at {i} to {update_idx}, before: {out:?}");
+                    out.remove(i);
+                    out.insert(update_idx, page);
+                    #[cfg(test)]
+                    println!("  After {out:?}")
+                }
+            }
+        }
+
+        Some(out)
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn test_example() {
+            let input = aoc::example::example_string("day5.txt");
+            let input = parse_input(&input);
+            println!("{:?}", input.rules_after);
+
+            assert_eq!(
+                fix(&[75, 97, 47, 61, 53], &input.rules_after).unwrap(),
+                vec![97, 75, 47, 61, 53]
+            );
+            assert_eq!(
+                fix(&[61, 13, 29], &input.rules_after).unwrap(),
+                vec![61, 29, 13]
+            );
+            assert_eq!(
+                fix(&[97, 13, 75, 29, 47], &input.rules_after).unwrap(),
+                vec![97, 75, 47, 29, 13]
+            );
+
+            assert_eq!(calculate(&input), 123);
+        }
+    }
+}
+
 fn main() {
     let cli = aoc::cli::parse();
 
@@ -91,4 +176,5 @@ fn main() {
     let input = parse_input(&input);
 
     println!("Part 1: {}", part1::calculate(&input));
+    println!("Part 2: {}", part2::calculate(&input));
 }
