4944bda14f2495026d3c80bb3089127c62cca563
Ben Tattersley <ubershinysheep@gmail.com>
2024-12-07 20:42:33 +0000

    Day 7 Part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -3,9 +3,43 @@ use std::iter;
 use aoc2024::aoc;
 use itertools::Itertools;
 
+type Op = fn(usize, usize) -> usize;
+
 struct Equation {
     result: usize,
     args: Vec<usize>,
+}
+
+impl Equation {
+    pub fn possible(&self, ops: &'static [Op]) -> bool {
+        self.evaluate(ops).contains(&self.result)
+    }
+
+    pub fn evaluate(&self, ops: &'static [Op]) -> impl Iterator<Item = usize> + '_ {
+        evaluate(&self.args, ops)
+    }
+}
+
+fn add(a: usize, b: usize) -> usize {
+    a + b
+}
+fn mul(a: usize, b: usize) -> usize {
+    a * b
+}
+fn concat(a: usize, b: usize) -> usize {
+    a * 10usize.pow(b.ilog10() + 1) + b
+}
+
+fn evaluate<'a>(args: &'a [usize], ops: &'static [Op]) -> Box<dyn Iterator<Item = usize> + 'a> {
+    match args {
+        [] => Box::new(iter::empty()),
+        [x] => Box::new(iter::once(*x)),
+        [head @ .., x] => Box::new(
+            evaluate(head, ops)
+                .map(move |n| ops.iter().map(move |op| op(n, *x)))
+                .flatten(),
+        ),
+    }
 }
 
 fn parse_input(s: &str) -> Vec<Equation> {
@@ -20,41 +54,13 @@ fn parse_input(s: &str) -> Vec<Equation> {
         .collect()
 }
 
-fn add(a: usize, b: usize) -> usize {
-    a + b
-}
-fn mul(a: usize, b: usize) -> usize {
-    a * b
-}
-
-fn evaluate<'a>(
-    args: &'a [usize],
-    ops: &'static [fn(usize, usize) -> usize],
-) -> Box<dyn Iterator<Item = usize> + 'a> {
-    match args {
-        [] => Box::new(iter::empty()),
-        [x] => Box::new(iter::once(*x)),
-        [head @ .., x] => Box::new(
-            evaluate(head, ops)
-                .map(move |n| ops.iter().map(move |op| op(n, *x)))
-                .flatten(),
-        ),
-    }
-}
-
 mod part1 {
     use super::*;
 
-    pub fn possible(equation: &Equation) -> bool {
-        evaluate(&equation.args, &[add, mul]).contains(&equation.result)
-    }
-
-    pub fn calculate(input: &str) -> usize {
-        let equations = parse_input(input);
-
+    pub fn calculate(equations: &[Equation]) -> usize {
         equations
             .iter()
-            .filter_map(|e| possible(e).then_some(e.result))
+            .filter_map(|e| e.possible(&[add, mul]).then_some(e.result))
             .sum()
     }
 
@@ -68,16 +74,19 @@ mod part1 {
             for val in evaluate(&[81, 40, 27], &[add, mul]) {
                 println!("{val}");
             }
-            assert_eq!(calculate(&input), 3749);
+            assert_eq!(calculate(&parse_input(&input)), 3749);
         }
     }
 }
-/*
+
 mod part2 {
     use super::*;
 
-    pub fn calculate(input: &str) -> usize {
-        0
+    pub fn calculate(equations: &[Equation]) -> usize {
+        equations
+            .iter()
+            .filter_map(|e| e.possible(&[add, mul, concat]).then_some(e.result))
+            .sum()
     }
 
     #[cfg(test)]
@@ -87,16 +96,17 @@ mod part2 {
         #[test]
         fn test_example() {
             let input = aoc::example::example_string("day7.txt");
-            assert_eq!(calculate(&input), 0);
+            assert_eq!(calculate(&parse_input(&input)), 11387);
         }
     }
 }
-*/
+
 fn main() {
     let cli = aoc::cli::parse();
 
     let input = cli.input_string();
+    let equations = parse_input(&input);
 
-    println!("Part 1: {}", part1::calculate(&input));
-    // println!("Part 2: {}", part2::calculate(&input));
+    println!("Part 1: {}", part1::calculate(&equations));
+    println!("Part 2: {}", part2::calculate(&equations));
 }
