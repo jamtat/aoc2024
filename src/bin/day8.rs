6ddda0606d0895d7f082027db5e37f32f1a02516
Ben Tattersley <ubershinysheep@gmail.com>
2024-12-13 00:36:06 +0000

    Day 8 Part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,30 +1,41 @@
 use aoc2024::{aoc, point2d::Point2D};
 use std::collections::{HashMap, HashSet};
 
-mod part1 {
+fn parse_input(
+    input: &str,
+) -> (
+    impl Fn(&Point2D<isize>) -> bool,
+    HashMap<char, Vec<Point2D<isize>>>,
+) {
+    let width = input.lines().next().unwrap().len() as isize;
+    let height = input.lines().count() as isize;
 
+    let frequency_points: HashMap<char, Vec<Point2D<isize>>> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            let y = y as isize;
+            line.chars().enumerate().filter_map(move |(x, c)| {
+                let x = x as isize;
+                (c != '.').then_some((c, Point2D::new(x, y)))
+            })
+        })
+        .fold(HashMap::new(), |mut acc, (c, point)| {
+            acc.entry(c).or_default().push(point);
+            acc
+        });
+
+    (
+        move |p: &Point2D<isize>| p.x >= 0 && p.x < width && p.y >= 0 && p.y < height,
+        frequency_points,
+    )
+}
+
+mod part1 {
     use super::*;
 
     pub fn calculate(input: &str) -> usize {
-        let width = input.lines().next().unwrap().len() as isize;
-        let height = input.lines().count() as isize;
-
-        let frequency_points: HashMap<char, Vec<Point2D<isize>>> = input
-            .lines()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                let y = y as isize;
-                line.chars().enumerate().filter_map(move |(x, c)| {
-                    let x = x as isize;
-                    (c != '.').then_some((c, Point2D::new(x, y)))
-                })
-            })
-            .fold(HashMap::new(), |mut acc, (c, point)| {
-                acc.entry(c).or_default().push(point);
-                acc
-            });
-
-        let in_bounds = |p: &Point2D<isize>| p.x >= 0 && p.x < width && p.y >= 0 && p.y < height;
+        let (in_bounds, frequency_points) = parse_input(input);
 
         let mut antinodes: HashSet<Point2D<isize>> = HashSet::new();
 
@@ -57,12 +68,43 @@ mod part1 {
         }
     }
 }
-/*
+
 mod part2 {
     use super::*;
 
     pub fn calculate(input: &str) -> usize {
-        0
+        let (in_bounds, frequency_points) = parse_input(input);
+
+        let mut antinodes: HashSet<Point2D<isize>> = HashSet::new();
+
+        for (_, points) in frequency_points {
+            for i in 0..points.len() - 1 {
+                for j in i + 1..points.len() {
+                    let p1 = points[i];
+                    let p2 = points[j];
+
+                    let diff = p2 - p1;
+
+                    for k in 0.. {
+                        let a = p1 - (diff * k);
+                        if in_bounds(&a) {
+                            antinodes.insert(a);
+                        } else {
+                            break;
+                        }
+                    }
+                    for k in 0.. {
+                        let a = p2 + (diff * k);
+                        if in_bounds(&a) {
+                            antinodes.insert(a);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        antinodes.into_iter().count()
     }
 
     #[cfg(test)]
@@ -72,16 +114,16 @@ mod part2 {
         #[test]
         fn test_example() {
             let input = aoc::example::example_string("day8.txt");
-            assert_eq!(calculate(&input), 0);
+            assert_eq!(calculate(&input), 34);
         }
     }
 }
-*/
+
 fn main() {
     let cli = aoc::cli::parse();
 
     let input = cli.input_string();
 
     println!("Part 1: {}", part1::calculate(&input));
-    // println!("Part 2: {}", part2::calculate(&input));
+    println!("Part 2: {}", part2::calculate(&input));
 }
