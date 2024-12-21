d3bb1d39751360ed2ce9e6a8a8118dd590c95366
Ben Tattersley <ubershinysheep@gmail.com>
2024-12-21 15:23:20 +0000

    Parse negative numbers

--- src/aoc/parse.rs
+++ src/aoc/parse.rs
@@ -1,11 +1,24 @@
-use nom;
-
 pub fn parse_number<T>(s: &str) -> nom::IResult<&str, T>
 where
     T: std::str::FromStr,
 {
     nom::combinator::map_res(
-        nom::bytes::complete::take_while1(|c: char| c.is_ascii_digit()),
-        |s: &str| s.parse::<T>(),
+        nom::combinator::recognize(nom::sequence::preceded(
+            nom::combinator::opt(nom::character::complete::char('-')),
+            nom::character::complete::digit1,
+        )),
+        str::parse::<T>,
     )(s)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::aoc::parse::parse_number;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(parse_number::<usize>("32").unwrap().1, 32usize);
+
+        assert_eq!(parse_number::<isize>("-64").unwrap().1, -64isize);
+    }
 }
