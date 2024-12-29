59328dbf6cbbc8c9223c32881e572cfee97faed7
Ben Tattersley <ubershinysheep@gmail.com>
2024-12-29 15:43:51 +0000

    Day 19 Part 2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,7 +1,8 @@
 use aoc2024::aoc;
+use std::collections::HashMap;
 
 mod part1 {
-    use std::collections::HashMap;
+    use super::*;
 
     fn possible(towels: &[String], pattern: &str, cache: &mut HashMap<String, bool>) -> bool {
         if !cache.contains_key(pattern) {
@@ -27,14 +28,9 @@ mod part1 {
 
         let patterns: Vec<String> = lines.map(str::to_string).collect();
 
-        println!("Towels: {:?}", towels);
-        println!("Patterns: {:?}", patterns);
-
-        let mut cache: HashMap<String, bool> = HashMap::new();
-
         patterns
             .iter()
-            .filter(|pattern| possible(&towels, pattern, &mut cache))
+            .filter(|pattern| possible(&towels, pattern, &mut HashMap::new()))
             .count()
     }
 
@@ -50,12 +46,49 @@ mod part1 {
         }
     }
 }
-/*
+
 mod part2 {
     use super::*;
 
+    fn arrangements(towels: &[String], pattern: &str, cache: &mut HashMap<String, usize>) -> usize {
+        if pattern.is_empty() {
+            return 0;
+        }
+        if !cache.contains_key(pattern) {
+            let count = towels
+                .iter()
+                .map(|towel| {
+                    if pattern == towel {
+                        1
+                    } else if pattern.starts_with(towel) {
+                        arrangements(towels, &pattern[towel.len()..], cache)
+                    } else {
+                        0
+                    }
+                })
+                .sum();
+            cache.insert(pattern.to_string(), count);
+        }
+        *cache.get(pattern).unwrap()
+    }
+
     pub fn calculate(input: &str) -> usize {
-        0
+        let mut lines = input.lines();
+        let towels: Vec<String> = lines
+            .next()
+            .unwrap()
+            .split(", ")
+            .map(str::to_string)
+            .collect();
+
+        lines.next();
+
+        let patterns: Vec<String> = lines.map(str::to_string).collect();
+
+        patterns
+            .iter()
+            .map(|pattern| arrangements(&towels, pattern, &mut HashMap::new()))
+            .sum()
     }
 
     #[cfg(test)]
@@ -65,16 +98,16 @@ mod part2 {
         #[test]
         fn test_example() {
             let input = aoc::example::example_string("day19.txt");
-            assert_eq!(calculate(&input), 0);
+            assert_eq!(calculate(&input), 16);
         }
     }
 }
-*/
+
 fn main() {
     let cli = aoc::cli::parse();
 
     let input = cli.input_string();
 
     println!("Part 1: {}", part1::calculate(&input));
-    // println!("Part 2: {}", part2::calculate(&input));
+    println!("Part 2: {}", part2::calculate(&input));
 }
